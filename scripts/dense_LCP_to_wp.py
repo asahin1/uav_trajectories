e2d2a86268ac006936a40f59d62fea987a6614f1
asahin1 <als421@lehigh.edu>
2023-03-21 11:24:24 -0400

    dense sampling of LCP during waypoint conversion

--- scripts/dense_LCP_to_wp.py
+++ scripts/dense_LCP_to_wp.py
@@ -8,6 +8,7 @@ if __name__ == "__main__":
   parser = argparse.ArgumentParser()
   parser.add_argument("input", type=str, help="txt file containing LCP")
   parser.add_argument("output", type=str, help="CSV file containing waypoints for the trajectory")
+  parser.add_argument("--n_sample", type=int, default=1, help="LCP sampling density")
   parser.add_argument("--x_off", type=float, default=0, help="mocap origin x offset")
   parser.add_argument("--y_off", type=float, default=0, help="mocap origin y offset")
   parser.add_argument("--z_off", type=float, default=0, help="mocap origin z offset")
@@ -23,17 +24,19 @@ if __name__ == "__main__":
       break
     line = line.replace('[','').replace(']','')
     if line[0] == '-':
-      csv_file = open('my_waypoints/'+args.output+str(pieces+1)+'.csv','w')
+      file_name = 'my_waypoints/'+args.output+'_'+str(pieces+1)+'.csv'
+      csv_file = open(file_name,'w')
       for line in txt_pieces[pieces]:
         csv_file.write(line)
       csv_file.close()
-      init_files.append('my_waypoints/'+args.output+str(pieces+1)+'.csv')
+      init_files.append(file_name)
       pieces += 1
       txt_pieces.append('')
     else:
       txt_pieces[pieces] += (line)
   txt_file.close()
 
+  pt_prev = None
   for file in init_files:
     data = np.loadtxt(file, delimiter=',', skiprows=0)
     data = data * 0.0254; ## inches to meter
@@ -43,5 +46,16 @@ if __name__ == "__main__":
     data = np.around(data,3)
     f = open(file,"w")
     writer = csv.writer(f)
-    for row in data:
-        writer.writerow(row)
+    it = data.shape[0]
+    for i in range(0,it-1):
+      p1 = data[i,:]
+      p2 = data[i+1,:]
+      diff = p2-p1
+      step = diff/args.n_sample
+      for j in range(0,args.n_sample):
+        pt = p1 + step*j
+        pt = np.around(pt,3)
+        if (pt != pt_prev).any():
+          writer.writerow(pt)
+        pt_prev = pt
+    writer.writerow(data[-1,:])
